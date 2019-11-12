2e3acac96757cf92937d2c48993b3d8165b67467
Sávio Rennan <saviorennan@gmail.com>
2019-11-12 16:09:01 -0300

    Consumindo e listando tweets com api do twitter - update api key

--- app/src/main/java/com/android/common/Const.kt
+++ app/src/main/java/com/android/common/Const.kt
@@ -8,7 +8,7 @@ const val OAUTH2_TOKEN_SUFFIX = "oauth2/token"
 const val SEARCH_SUFFIX = "1.1/search/tweets.json"
 
 //Twitter Api
-const val CONSUMER_API_KEY = "9txE4whc5obebtcUpZd8JEaHz"
+const val CONSUMER_API_KEY = "k718SW31ef44meXuyyRvWIebt"
 const val CONSUMER_API_SECRET_KEY = "X0e8mdsAdUC3HVWza7mR4W76VT3RjjYQLg2RQHsGegnEDkBAjB"
 const val BEARER_TOKEN_CREDENTIALS = "$CONSUMER_API_KEY:$CONSUMER_API_SECRET_KEY"
 const val GRANT_TYPE = "client_credentials"
